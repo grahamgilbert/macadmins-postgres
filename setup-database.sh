180fc2071bc314fde163f9b43f49fccb72a3e82d
Graham Gilbert <graham@grahamgilbert.com>
2014-11-10 18:21:40 +0000

    just exit 0, as this will fail if everything already exists

--- setup-database.sh
+++ setup-database.sh
@@ -6,4 +6,5 @@ gosu postgres postgres --single <<- EOSQL
    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 echo ""
-echo "******DOCKER DATABASE CREATED******
+echo "******DOCKER DATABASE CREATED******"
+exit 0
