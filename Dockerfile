55f3edac0cbe0a5326e13e3cd1f39456ab09b490
Graham Gilbert <graham@grahamgilbert.com>
2014-11-11 07:06:30 +0000

    Need to chmod the script

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ ENV DB_NAME database
 ENV DB_USER admin
 ENV DB_PASS password
 ADD setup-database.sh /docker-entrypoint-initdb.d/
+RUN chmod 755 /docker-entrypoint-initdb.d/setup-database.sh
--- setup-database.sh
+++ setup-database.sh
@@ -2,9 +2,14 @@
 echo "******CREATING DOCKER DATABASE******"
 gosu postgres postgres --single <<- EOSQL
    CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB;
+EOSQL
+
+gosu postgres postgres --single <<- EOSQL
    CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0 ENCODING 'UTF8';
+EOSQL
+
+gosu postgres postgres --single <<- EOSQL
    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 echo ""
 echo "******DOCKER DATABASE CREATED******"
-exit 0
