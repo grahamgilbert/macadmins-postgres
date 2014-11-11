0f29728a0258978894d9c4014cc554a0654ded9d
Graham Gilbert <graham@grahamgilbert.com>
2014-11-11 08:05:56 +0000

    Try testing for db before creation

--- setup-database.sh
+++ setup-database.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+TEST=`gosu postgres postgres --single <<- EOSQL
+   SELECT 1 FROM pg_database WHERE datname='$DB_NAME';
+EOSQL`
+
 echo "******CREATING DOCKER DATABASE******"
+if [[ $TEST == "1" ]]; then
+    # database exists
+    # $? is 0
+    exit 0
+else
 gosu postgres postgres --single <<- EOSQL
    CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB;
 EOSQL
@@ -11,6 +21,7 @@ EOSQL
 gosu postgres postgres --single <<- EOSQL
    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
+fi
+
 echo ""
 echo "******DOCKER DATABASE CREATED******"
-exit 0
