f62b26a8fb0483ab40b836088a6834eb0f275d83
Graham Gilbert <graham@grahamgilbert.com>
2015-10-18 10:28:27 +0100

    Fixing init issues

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM postgres:9.3.9
+FROM postgres:9.3.10
 ENV DB_NAME database
 ENV DB_USER admin
 ENV DB_PASS password
 ADD setup-database.sh /docker-entrypoint-initdb.d/
+#ADD docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod 755 /docker-entrypoint-initdb.d/setup-database.sh
+#
--- setup-database.sh
+++ setup-database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEST=`gosu postgres postgres --single <<- EOSQL
+TEST=`psql -U postgres <<-EOSQL
    SELECT 1 FROM pg_database WHERE datname='$DB_NAME';
 EOSQL`
 
@@ -10,15 +10,15 @@ if [[ $TEST == "1" ]]; then
     # $? is 0
     exit 0
 else
-gosu postgres postgres --single <<- EOSQL
+psql -U postgres <<-EOSQL
    CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB;
 EOSQL
 
-gosu postgres postgres --single <<- EOSQL
+psql -U postgres <<-EOSQL
    CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0 ENCODING 'UTF8';
 EOSQL
 
-gosu postgres postgres --single <<- EOSQL
+psql -U postgres <<-EOSQL
    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 fi
